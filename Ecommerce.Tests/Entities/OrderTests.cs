82d4e945b96497fce42165d99ac68c30e952309e
joao-pedro-garcia <jpedrooo.cg@gmail.com>
2026-02-05 17:50:40 -0300

    tests: AddOrderItem test

---
+++ Ecommerce.Tests/Entities/OrderTests.cs
@@ -0,0 +1,51 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Tests.Entities;
+
+public class OrderTests
+{
+     
+
+    [Fact]
+    public void AddOrderItem_WithInvalidPrice_ThenShouldThrowException()
+    {
+        var order = new Order();
+        
+        var act = () => order.AddOrderItem(2, "Mouse", 0);
+        
+        Assert.Throws<ArgumentOutOfRangeException>(act);
+    }
+    
+    [Fact]
+    public void AddOrderItem_WithInvalidQuantity_ThenShouldThrowException()
+    {
+        var order = new Order();
+        
+        var act = () => order.AddOrderItem(0, "Monitor", 10);
+        
+        Assert.Throws<ArgumentOutOfRangeException>(act);
+    }
+    
+    [Fact]
+    public void AddOrderItem_WithInvalidName_ThenShouldThrowException()
+    {
+        var order = new Order();
+        
+        var act = () => order.AddOrderItem(2, "", 10);
+        
+        Assert.Throws<ArgumentNullException>(act);
+    }
+    
+    [Fact]
+    public void AddOrderItem_WithValidParameters_ThenShouldAddItem()
+    {
+        var order = new Order();
+        
+        order.AddOrderItem(2, "Teclado", 522.12m);
+        
+        Assert.Single(order.OrderItems);
+    }
+
+    
+}
