f48ada54493c00f875b137e8f6293e69d0cdb71a
joao-pedro-garcia <jpedrooo.cg@gmail.com>
2026-02-05 17:51:11 -0300

    tests: update and cancel order tests

--- Ecommerce.Tests/Entities/OrderTests.cs
+++ Ecommerce.Tests/Entities/OrderTests.cs
@@ -46,6 +46,74 @@ public class OrderTests
         
         Assert.Single(order.OrderItems);
     }
-
+    [Fact]
+    public void UpdateOrder_WithValidItemId_ThenShouldUpdateItem()
+    {
+        var order = new Order();
+        var itemId = 0;
+        order.AddOrderItem(1, "Mouse", 10);
+        
+        order.UpdateOrder(2, "Mouse", 25, itemId); 
+        
+        var updated = order.OrderItems.First();
+        Assert.Equal(2, updated.Quantity);
+        Assert.Equal(25, updated.Price);
+        Assert.NotNull(order.UpdatedAt); 
+    }
+    
+    [Fact]
+    public void UpdateOrder_WithInvalidItemId_ThenShouldThrowException()
+    {
+        var order = new Order();
+        var itemId = 5;
+        order.AddOrderItem(1, "Mouse", 10);
+        
+        var act = () => order.UpdateOrder(2, "Mouse", 25, itemId); 
+        
+        Assert.Throws<ArgumentException>(act);
+    }
+    
+    [Theory]
+    [InlineData(Status.Canceled)]
+    [InlineData(Status.Processed)]
+    public void UpdateOrder_WithInvalidStatus_ThenShouldThrowException(Status status)
+    {
+        var order = new Order();
+        typeof(Order)
+            .GetProperty("Status")!
+            .SetValue(order, status);        
+        var itemId = 0;
+        order.AddOrderItem(1, "Mouse", 10);
+        
+        var act = () => order.UpdateOrder(2, "Mouse", 25, itemId); 
+        
+        Assert.Throws<InvalidOperationException>(act);
+    }
+    
+    [Fact]
+    public void CancelOrder_WithValidParams_ThenShouldCancelOrder()
+    {
+        var order = new Order();
+        order.AddOrderItem(2, "Teclado", 522.12m);
+        
+        order.CancelOrder();
+        
+        Assert.Equal(Status.Canceled, order.Status);
+    }
+    
+    [Fact]
+    public void CancelOrder_WithInvalidStatus_ThenShouldThrowExceptionr()
+    {
+        var order = new Order();
+        typeof(Order)
+            .GetProperty("Status")!
+            .SetValue(order, Status.Canceled);       
+        order.AddOrderItem(2, "Teclado", 522.12m);
+        
+        var act = () => order.CancelOrder();
+        
+        
+        Assert.Throws<InvalidOperationException>(act);
+    }
     
 }
