460e66d0502d45f7c9dce7bbc14291e0a9811cb2
joao-pedro-garcia <jpedrooo.cg@gmail.com>
2026-02-05 18:04:02 -0300

    feat: add entities mapping and dbcontext

---
+++ Ecommerce.Infrastructure/Database/Context/EcommerceDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Database.Context;
+
+public class EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : DbContext(options)
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OrderItem>();
+        modelBuilder.Entity<Order>();
+    }
+}
---
+++ Ecommerce.Infrastructure/Database/Mapping/OrderMap.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Database.Mapping;
+
+public class OrderMap : IEntityTypeConfiguration<Order> 
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+        
+        builder.Property(o => o.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+        
+        builder.Property(o => o.CreatedAt).HasColumnName("CreatedAt").HasDefaultValueSql("getdate()").IsRequired();
+        builder.Property(o => o.UpdatedAt).HasColumnName("UpdatedAt");
+        builder.Property(o => o.Status).HasColumnName("Status").IsRequired();
+        
+        builder.HasMany(o => o.OrderItems).WithOne(o => o.Order).HasForeignKey(o => o.OrderId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+        
+    }
+}
