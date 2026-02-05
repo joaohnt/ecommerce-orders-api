a8a0aa98542e608a38d5ae6178200b9426ab8a2a
joao <124461386+joaohnt@users.noreply.github.com>
2026-02-05 17:47:38 -0300

    Feature/domain model (#1)
    * feat: add domain entities and enum 
    
    * feat: add rules

---
+++ Ecommerce.Domain/Entities/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿namespace Ecommerce.Domain.Entities;
+
+public class OrderItem
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public int Quantity { get; private set; }
+    
+    public int OrderId { get; private set; }
+    public Order Order { get; private set; }
+    
+    internal OrderItem(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+    
+    internal void Update(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+}
---
+++ Ecommerce.Domain/Enums/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Domain.Enums;
+
+public enum Status
+{
+    Received,
+    Processed,
+    Canceled,
+}
